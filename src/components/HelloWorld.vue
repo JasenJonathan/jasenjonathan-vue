8b854210cb6ef0412b7a79e53f189adc1a9ae428
JasenJonathan <kagaminomae@gmail.com>
2021-11-14 19:37:50 +0700

    01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,9 +13,6 @@
       <div class="carousel-item">
         <img src="GenGroup.jpg" class="d-block w-100" alt="...">
       </div>
-      <div class="carousel-item">
-        <img src="KeQing2.jpg" class="d-block w-100" alt="...">
-      </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
