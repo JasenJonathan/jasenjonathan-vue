ec269f88387c7b14be470aeba8a1ff6da0dae527
JasenJonathan <kagaminomae@gmail.com>
2021-11-17 19:19:17 +0700

    up 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,9 +23,9 @@
           </h2>
         <h3>
           saya seorang mahasiswa dari Universitas Multi Data Palembang.<br>
-          18 tahun<br>
+          saya kebanyakan di rumah, jadi malas keluar<br>
           NEET<br>
-          beragama buddha
+          saya dulu ingin menjadi dokter tetapi tidak kuat melihat darah jadi berubah.
         </h3>
       </div>
       <div class="carousel-item">
@@ -37,19 +37,19 @@
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">Hobby</th>
+                <th scope="col">What i do</th>
                 <th scope="col">Achievement</th>
               </tr>
             </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
-              <td>Reading Manga</td>
+              <td>Game Design</td>
               <td>None</td>
             </tr>
             <tr>
               <th scope="row">2</th>
-              <td>Overthinking</td>
+              <td>Game Programming</td>
               <td>None</td>
             </tr>
             <tr>
@@ -66,7 +66,15 @@
         <div class="img-container">
           <img src="gear.png">
         </div>
+        <h2>More About me</h2>
+        <h2>Nama saya jasen jonathan, saya berumur 18 tahun</h2>
+        <h2>Saya lahir pada tanggal 24 Januari 2003</h2>
+        <h2>Tinggal di Jl. MP Mangkunegara No.04</h2>
+        <h2>Email : jasenjonathan123@gmail.com</h2>
+        <h2>No. Telp : 082246847270</h2>
+        <button type="button" class="button">Contact Me</button>
       </div>
+      
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
